768fe0f88011bafd988db6678b4827924ac7a2c1
levixticus67-lab <levixticus67@gmail.com>
2026-07-18 21:06:34 +0300

    feat: add My Family to Workforce portal

---
+++ artifacts/lugazi/src/pages/workforce/Family.tsx
@@ -0,0 +1,6 @@
+import FamilyPage from "@/components/FamilyPage";
+import { workforceNavItems } from "./navItems";
+
+export default function WorkforceFamily() {
+  return <FamilyPage navItems={workforceNavItems} />;
+}
