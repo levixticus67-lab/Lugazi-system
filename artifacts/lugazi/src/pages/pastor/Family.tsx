4e0d698f98b63d2c0701866eeb763b9b49ec2ef5
levixticus67-lab <levixticus67@gmail.com>
2026-07-18 21:06:33 +0300

    feat: add My Family to Pastor portal

---
+++ artifacts/lugazi/src/pages/pastor/Family.tsx
@@ -0,0 +1,6 @@
+import FamilyPage from "@/components/FamilyPage";
+import { pastorNavItems } from "./navItems";
+
+export default function PastorFamily() {
+  return <FamilyPage navItems={pastorNavItems} />;
+}
