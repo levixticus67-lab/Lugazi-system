7432b24bbb863d47b458734d00d1cda8f440ee38
levixticus67-lab <levixticus67@gmail.com>
2026-07-18 21:06:35 +0300

    feat: add My Family to Admin portal

---
+++ artifacts/lugazi/src/pages/admin/Family.tsx
@@ -0,0 +1,6 @@
+import FamilyPage from "@/components/FamilyPage";
+import { adminNavItems } from "./navItems";
+
+export default function AdminFamily() {
+  return <FamilyPage navItems={adminNavItems} />;
+}
