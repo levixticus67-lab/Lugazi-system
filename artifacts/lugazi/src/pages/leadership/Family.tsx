ed9b970f6c82d4617c14feb249e310d3db18e26d
levixticus67-lab <levixticus67@gmail.com>
2026-07-18 21:06:33 +0300

    feat: add My Family to Leadership portal

---
+++ artifacts/lugazi/src/pages/leadership/Family.tsx
@@ -0,0 +1,6 @@
+import FamilyPage from "@/components/FamilyPage";
+import { leadershipNavItems } from "./navItems";
+
+export default function LeadershipFamily() {
+  return <FamilyPage navItems={leadershipNavItems} />;
+}
